7967cc5c7772e3f95f0bffc88ac60639d05a304d
Katie Horne <katie@coder.com>
2021-05-25 11:18:49 -0500

    Create storenames.js

---
+++ storenames.js
@@ -0,0 +1,7 @@
+# Courtesy of FreeCodeCamp
+
+function storeNames() { return arguments; }
+
+// If we execute the following line in the console:
+storeNames("Mulder", "Scully", "Alex Kryceck");
+// The output will be { '0': 'Mulder', '1': 'Scully', '2': 'Alex Kryceck' }
